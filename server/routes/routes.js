a334e602d0e9c61b7cac4e0c2c24f26fad9b8ab2
Raulscj <92169825+Raulscj@users.noreply.github.com>
2023-02-23 13:08:59 -0400

    ruta inicial

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-router.get("/", (req, res) => {
+/* router.get("/", (req, res) => {
   res.send("Good");
+}); */
+router.get("/", (req, res) => {
+  res.redirect("http://localhost:5173");
 });
+
 module.exports = router;
