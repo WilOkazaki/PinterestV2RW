628abe7dca0ae638a1f7264566fb0bcd2a1e1898
Raulscj <92169825+Raulscj@users.noreply.github.com>
2023-02-24 15:10:50 -0400

    .gitignore solventado

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-/* router.get("/", (req, res) => {
-  res.send("Good");
-}); */
 router.get("/", (req, res) => {
-  res.redirect("http://localhost:5173");
+  res.send("Good");
 });
+/* router.get("/", (req, res) => {
+  res.redirect("http://localhost:5173");
+}); */
 
 module.exports = router;
