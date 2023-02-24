9d0021bcd3c334e5aaf933f31430389ff031d62e
Raulscj <radscjggdiosteama@gmail.com>
2023-02-24 02:32:00 -0400

    toca dormir

--- server/database.js
+++ server/database.js
@@ -5,20 +5,22 @@ const sequelize = new Sequelize("my_database", "root", "AdminUser", {
   host: "localhost",
   dialect: "mysql",
 });
+//nombre de la base de datos
+const databaseName = "my_database";
 
+const createDatabaseQuery = `CREATE DATABASE IF NOT EXISTS ${databaseName}`;
 // Función que ejecuta la consulta SQL para crear la base de datos si aún no existe
 async function createDatabase() {
   try {
-    await sequelize.query("CREATE DATABASE IF NOT EXISTS my_database");
-    console.log("Base de datos 'my_database' creada exitosamente");
+    await sequelize.query(createDatabaseQuery);
+    console.log(`Base de datos '${databaseName}' creada exitosamente`);
   } catch (error) {
-    console.error("Error al crear la base de datos 'my_database':", error);
+    console.error(`Error al crear la base de datos '${databaseName}':`, error);
   } finally {
     // Cierra la conexión a la base de datos
     await sequelize.close();
   }
 }
-
 // Ejecuta la función para crear la base de datos
 createDatabase();
 
