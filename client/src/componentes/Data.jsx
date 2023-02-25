747769146dd9946d582b60c99ec2ebe4ca12e349
WilOkazaki <okazakisaito2012@gmail.com>
2023-02-25 14:42:19 -0500

    campos que acompañan a la imagen

--- client/src/componentes/Data.jsx
+++ client/src/componentes/Data.jsx
@@ -1,18 +1,18 @@
 const Data=[{
     id:1,
     imgSrc:
-    "https://cdn.pixabay.com/photo/2016/02/28/12/55/boy-1226964_1280.jpg",
-    name:"Niño en el cercado",
-    link:"https://pixabay.com/es/photos/chico-valla-pobreza-hambriento-1226964/",
+    "https://cdn.pixabay.com/photo/2023/01/31/05/59/zebra-7757193_1280.jpg",
+    name:"Zebra",
+    link:"https://cdn.pixabay.com/photo/2023/01/31/05/59/zebra-7757193_1280.jpg",
     size:"small",
 },
 {
 
     id:2,
     imgSrc:
-    "https://cdn.pixabay.com/photo/2023/02/21/19/10/seagull-7804897_1280.jpg",
-    name:"Router 2023",
-    link:"",
+    "https://cdn.pixabay.com/photo/2023/02/02/13/27/cat-7762887_1280.jpg",
+    name:"Gatos",
+    link:"https://cdn.pixabay.com/photo/2023/02/02/13/27/cat-7762887_1280.jpg",
     size:"medium"
 
 
@@ -21,9 +21,9 @@ const Data=[{
 
     id:3,
     imgSrc:
-    "https://cdn.pixabay.com/photo/2016/07/29/14/34/ballet-1553361_1280.jpg",
-    name:"ballet",
-    link:"https://pixabay.com/es/photos/ballet-zapatilla-vestir-1553361/",
+    "https://cdn.pixabay.com/photo/2023/01/30/20/02/bird-7756521_1280.jpg",
+    name:"Pajaro",
+    link:"https://cdn.pixabay.com/photo/2023/01/30/20/02/bird-7756521_1280.jpg",
     size:"large"
 
 
